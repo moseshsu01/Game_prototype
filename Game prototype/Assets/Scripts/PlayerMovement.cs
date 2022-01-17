0658ec4a19af9b559c878f7f67601bab15cbf6df
Moses Hsu <moses@zaber.com>
2022-01-16 23:25:57 -0800

    implement player movement

--- Game prototype/Assets/Scripts/PlayerMovement.cs
+++ Game prototype/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,78 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed;
+    private Rigidbody2D rb;
+    private enum Direction
+    {
+        left, right, up, down
+    }
+    private List<Direction> directionInputs = new();
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("left"))
+        {
+            directionInputs.Add(Direction.left);
+        }
+        if (Input.GetKeyDown("right"))
+        {
+            directionInputs.Add(Direction.right);
+        }
+        if (Input.GetKeyDown("up"))
+        {
+            directionInputs.Add(Direction.up);
+        }
+        if (Input.GetKeyDown("down"))
+        {
+            directionInputs.Add(Direction.down);
+        }
+
+        if (Input.GetKeyUp("left"))
+        {
+            directionInputs.Remove(Direction.left);
+        }
+        if (Input.GetKeyUp("right"))
+        {
+            directionInputs.Remove(Direction.right);
+        }
+        if (Input.GetKeyUp("up"))
+        {
+            directionInputs.Remove(Direction.up);
+        }
+        if (Input.GetKeyUp("down"))
+        {
+            directionInputs.Remove(Direction.down);
+        }
+
+        Vector2 movement = Vector2.zero;
+        if (directionInputs.Count > 0)
+        {
+            switch (directionInputs[^1])
+            {
+                case Direction.left:
+                    movement = new Vector2(-speed, 0);
+                    break;
+                case Direction.right:
+                    movement = new Vector2(speed, 0);
+                    break;
+                case Direction.up:
+                    movement = new Vector2(0, speed);
+                    break;
+                case Direction.down:
+                    movement = new Vector2(0, -speed);
+                    break;
+            }
+        }
+
+        if (rb.velocity != movement)
+        {
+            rb.velocity = movement;
+        }
     }
 }
