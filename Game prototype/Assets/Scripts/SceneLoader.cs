0bd08475dac8679ca86af9e1b00748c813510126
Moses Hsu <67880280+moseshsu01@users.noreply.github.com>
2022-06-05 15:00:48 -0700

    implement super basic scene transition

--- Game prototype/Assets/Scripts/SceneLoader.cs
+++ Game prototype/Assets/Scripts/SceneLoader.cs
@@ -5,13 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public string sceneToLoad;
+    [SerializeField] private string sceneToLoad;
+    [SerializeField] private GameObject fadeInImage;
+    private float fadeWait = 0.5f;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene(sceneToLoad);
+            collision.gameObject.GetComponent<PlayerMovement>().freezeMovement();
+            StartCoroutine(fadeIn());
         }
+    }
+
+    private IEnumerator fadeIn()
+    {
+        Instantiate(fadeInImage, Vector3.zero, Quaternion.identity);
+        yield return new WaitForSeconds(fadeWait);
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
